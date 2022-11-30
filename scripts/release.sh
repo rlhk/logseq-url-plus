847e3ff3c22681555bb0579a226aafd1509fa9cd
Roger Lam <rlhk.open@gmail.com>
2022-11-30 17:44:02 +0800

    Simpler release.sh using jet built-in options

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # brew install borkdude/brew/jet # uncomment to install the required jet tool
-VERSION=`cat package.json | jet -i json --keywordize -q ':version' -f '#(println %)'`
+VERSION=`cat package.json | jet -i json --keywordize -q ':version println'` 
 git tag -a $VERSION -m "Release version: $VERSION"
 git push origin $VERSION
