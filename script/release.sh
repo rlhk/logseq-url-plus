0df37539d6cc602af7d55b0286b156a54801fc86
Roger Lam <rlhk.open@gmail.com>
2022-12-26 04:59:28 +0800

    Simplify bb task scripts.

--- script/release.sh
+++ script/release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# NOTE: Obsolete script. See `release` task in `bb.edn`
 # brew install borkdude/brew/jet # uncomment to install the required jet tool
 # `package.json` is the single source of truth of version no.
 VERSION=`cat package.json | jet -i json -k -q ':version println'` 
--- scripts/helper.clj
+++
@@ -1,8 +0,0 @@
-(ns helper
-  (:require [babashka.process :refer [shell]]))
-
-(defn run [& cmds] (doseq [cmd cmds] (shell cmd)))
-
-(comment
-  (run "pwd" "whoami")
-  )
