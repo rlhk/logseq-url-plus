46aa23e7c42e01b6ef4d10cff5c79f44b3fc025d
Roger Lam <rlhk.open@gmail.com>
2023-01-15 01:04:18 +0800

    tailwindcss config.

--- src/main/ui.cljs
+++ src/main/ui.cljs
@@ -4,14 +4,19 @@
 #_{:clj-kondo/ignore [:unresolved-symbol]}
 (rum/defc plugin-panel []
   (println "Mounting logseq url+ advanced UI ...")
-  [:div
-   [:div.text
-    {:style {:background-color "lightgray"
-             :color "darkgray"
-             :font-family "system-ui"}}
-    "Hi URL+ user!"]
-   [:button {:class "btn btn-primary"
-             :on-click #(do (println "clicked...")
-                            (js/logseq.hideMainUI))}
-    "Click Me!!"]])
+  [:main.fixed.inset-0.flex.items-center.justify-center
+   [:div.url-plus-box
+    [:div
+     [:div.radial-progress {:style {"--value" "70"}} "70%"]
+     [:div.text "Hi URL+ user!"]
+     [:.flex.w-full
+      [:button {:class "btn btn-outline btn-sm btn-info"
+                :on-click #(do (println "clicked...")
+                               (js/logseq.hideMainUI))}
+       "Confirm"]
+      [:.divider.divider-horizontal]
+      [:button {:class "btn btn-outline btn-sm btn-outline"
+                :on-click #(do (println "clicked...")
+                               (js/logseq.hideMainUI))}
+       "Cancel"]]]]])
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const { transparent } = require('daisyui/src/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      padding: '2rem'
+    }
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    // base: false,
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          'main': {
+            'background-color': transparent,
+          }
+      }
+    }],
+  }
 }
